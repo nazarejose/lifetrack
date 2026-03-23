eb36a20af1eeb149562501bd39a75e9646e88f00
nazarejose <nazarejosecontact@gmail.com>
2026-03-23 01:19:29 -0300

    feat: CORS

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,7 +14,10 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
  
-  app.enableCors();
+  app.enableCors({
+    origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
     forbidNonWhitelisted: true,
